e6160ecd247a9a1e56dcb00ed7747490a5129f99
Icanfly37 <101133532+Icanfly37@users.noreply.github.com>
2023-09-28 17:59:11 +0700

    Revert "Please, test_code before dev if doesn't work, Revert it"
    This reverts commit a852fad37e0ed9cda43be1c886b9cebea0e80274.

--- project/lib/Modules/ManagerPage/Teachingtime/timesubject.dart
+++ project/lib/Modules/ManagerPage/Teachingtime/timesubject.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:ku_t/Modules/ManagerPage/Teachingtime/PopUp.dart';
 
-import '../../Component/Header_web.dart';
+import '../../Componemt/Header_web.dart';
 
 class Addtime extends StatefulWidget {
   const Addtime({Key? key}) : super(key: key);
