cd8841c4e8013df14a676df5daef0fe8089681a7
Patchareeporn Sakprom <patchareeporn.s@ku.th>
2023-10-25 14:40:37 +0700

    Updated

--- project/lib/Modules/Data/data_test.dart
+++ project/lib/Modules/Data/data_test.dart
@@ -1,4 +1,5 @@
 List<String> creditDropdown = ["1(0-3-2)", "2(2-0-4)", "3(3-0-6)", "4(3-3-8)"];
+List<String> yearDropdown = ["T12(1)", "T12(2)", "T12(3)", "T12(4)"];
 
 List<Map<String, dynamic>> data = [
   {
@@ -6,35 +7,41 @@ List<Map<String, dynamic>> data = [
     'name_subject': 'Engineering Mathematics I',
     'credit': creditDropdown[2],
     'pre': '',
+    'year': yearDropdown[0],
   },
   {
     'id': '01420111-60',
     'name_subject': 'General Physics I',
     'credit': creditDropdown[2],
     'pre': '',
+    'year': yearDropdown[0],
   },
   {
     'id': '01420113-60',
     'name_subject': 'Laboratory in Physics I',
     'credit': creditDropdown[0],
     'pre': '',
+    'year': yearDropdown[0],
   },
   {
     'id': '01999111-60',
     'name_subject': 'Knowledge of the Land',
     'credit': creditDropdown[1],
     'pre': '',
+    'year': yearDropdown[0],
   },
   {
     'id': '03603111-60',
     'name_subject': 'Programming Fundamentals I',
     'credit': creditDropdown[2],
     'pre': '',
+    'year': yearDropdown[0],
   },
   {
     'id': '03604111-60',
     'name_subject': 'Engineering Drawing',
     'credit': creditDropdown[2],
     'pre': '',
+    'year': yearDropdown[0],
   },
 ];
