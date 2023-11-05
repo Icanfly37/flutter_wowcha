4e4cb37f235acfe8c8368bc69818df47be24ef49
itnisa-a <jennisa.r@ku.th>
2023-11-06 01:19:22 +0700

    Update TimetablePage.dart

--- project/lib/Modules/ManagerPage/Timetable/TimetablePage.dart
+++ project/lib/Modules/ManagerPage/Timetable/TimetablePage.dart
@@ -3,6 +3,7 @@ import 'dart:js_interop_unsafe';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ku_t/Modules/Component/Header_web.dart';
+import 'package:ku_t/Modules/Component/showDialogSubject.dart';
 import 'package:ku_t/Modules/Dropdown/TeachernameModel.dart';
 import 'package:ku_t/Modules/Dropdown/TimeEndModel.dart';
 import 'package:ku_t/Modules/Dropdown/TimeStartModel.dart';
@@ -335,17 +336,23 @@ class _TimetableState extends State<Timetable> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TimetableView(
-        laneEventsList: weekEvents,
-        onEventTap: onEventTapCallBack,
-        timetableStyle: const TimetableStyle(
-          timeItemHeight: 30,
-          laneWidth: 170,
-          laneHeight: 50,
-          startHour: 8,
-          endHour: 21,
-          timeItemTextColor: Colors.black,
-          visibleTimeBorder: false,
+      body: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 1, color: Color.fromRGBO(0, 102, 94, 1),),
+        ),
+        child: TimetableView(
+          laneEventsList: weekEvents,
+          onEventTap: onEventTapCallBack,
+          timetableStyle: const TimetableStyle(
+            timeItemHeight: 35,
+            laneWidth: 170,
+            laneHeight: 40,
+            startHour: 8,
+            endHour: 21,
+            timeItemTextColor: Colors.black,
+            visibleTimeBorder: false,
+          ),
+          onEmptySlotTap: onTimeSlotTappedCallBack,
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -565,30 +572,7 @@ class _TimetableState extends State<Timetable> {
                             ),
                           ],
                         ),
-            Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              alignment: Alignment.bottomLeft,
-                              child: Text(
-                                e.title!,
-                                style: textStylehint(),
-                              ),
-                            ))
-                        .toList(),
-                    onChanged: (value) {
-                      var parts = value!.split(':');
-                      if (parts.length == 2) {
-                        newEvent.startTime = TableEventTime(
-                          hour: int.parse(parts[0]),
-                          minute: int.parse(parts[1]),
-                        );
-                      }
-                    },
-                  ),
-                ],
-              ),
-              Column(
+                        Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
@@ -756,25 +740,8 @@ class _TimetableState extends State<Timetable> {
         }
       );
     }
-  }
 
-  List<LaneEvents> _buildWeekEvents() {
-    List<LaneEvents> weekEvents = [];
-
-    // Define the names of the days
-    List<String> days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-    for (int day = 0; day < 7; day++) { // 7 days for Monday to Sunday
-      weekEvents.add(LaneEvents(
-        lane: Lane(name: days[day], laneIndex: 1,textStyle: textStylehintbold()),
-        events: [],
-      ));
-    }
-
-    return weekEvents;
-  }
-
-  void onEventTapCallBack(TableEvent event) {
+    void onEventTapCallBack(TableEvent event) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -801,33 +768,29 @@ class _TimetableState extends State<Timetable> {
         );
       },
     );
+
+    
   }
-}
-
-List<LaneEvents> _buildWeekEvents() {
-  List<LaneEvents> weekEvents = [];
-
-  // Define the names of the days
-  List<String> days = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
-  ];
-
-  for (int day = 0; day < 7; day++) {
-    // 7 days for Monday to Sunday
-    weekEvents.add(LaneEvents(
-      lane: Lane(name: days[day], laneIndex: 1, textStyle: textStylehintbold()),
-      events: [],
-    ));
   }
 
-  return weekEvents;
-}
+  List<LaneEvents> _buildWeekEvents() {
+    List<LaneEvents> weekEvents = [];
+
+    // Define the names of the days
+    List<String> days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    for (int day = 0; day < 7; day++) { // 7 days for Monday to Sunday
+      weekEvents.add(LaneEvents(
+        lane: Lane(name: days[day], laneIndex: 1,textStyle: textStylehintbold()),
+        events: [],
+      ));
+    }
+
+    return weekEvents;
+  }
+
+  
+
 
 void onTimeSlotTappedCallBack(
     int laneIndex, TableEventTime start, TableEventTime end) {
